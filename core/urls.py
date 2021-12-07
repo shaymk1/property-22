a0f31b8a9a5e141ecfc188769346b0a41a1b263f
shaymk1 <mkekae@gmail.com>
2021-12-07 11:36:57 +0200

    added listings and realtors apps, urls and templates

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('about/', include('pages.urls')),
+    path('listings/', include('listings.urls')),
+    path('realtors/', include('realtors.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
